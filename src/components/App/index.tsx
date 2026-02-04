2a04889003f4cc44e6ee2b0d2a7b969eb588ed52
RenatAllakhyarov <allakhyarovrenat@gmail.com>
2026-02-04 13:16:11 +0300

    [CT-50] fix: Removed testing app code

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,39 +1,9 @@
-import LivingEntity from "@domains/LivingEntity";
-import { useEffect, useMemo, useState, type ReactElement } from "react";
+import { ReactElement } from "react";
 import "./style.css";
 
 const App = (): ReactElement => {
-    const testEntity = useMemo(() => new LivingEntity("MotherFucker"), []);
-
-    const [state, setState] = useState({
-        hungerPoints: testEntity.getHungerPoints(),
-        healthPoints: testEntity.getHealthPoints(),
-        emotion: testEntity.getEmotion(),
-    });
-
-    const handleChange = () => {
-        console.log("FEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEED");
-    };
-
-    useEffect(() => {
-        console.log("REREEEEEENDEEEER");
-        const interval = setInterval(() => {
-            setState({
-                hungerPoints: testEntity.getHungerPoints(),
-                healthPoints: testEntity.getHealthPoints(),
-                emotion: testEntity.getEmotion(),
-            });
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <div>
-            <div>HUNGER: {state.hungerPoints}</div>
-            <div>HEALTH: {state.healthPoints}</div>
-            <div>EMOTION: {state.emotion}</div>
-            {/* <button onClick={handleChange}>FEED</button> */}
         </div>
     );
 };
